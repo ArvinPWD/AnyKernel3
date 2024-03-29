e19be9d534719ad2a607b79477889c1af4ed6216
Arvin <159819760+ArvinHPWD@users.noreply.github.com>
2024-03-30 05:33:11 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,15 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=MaVins Kernel by Uke@PWD
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=merlin
+device.name2=merlinx
+device.name3=lancelot
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
