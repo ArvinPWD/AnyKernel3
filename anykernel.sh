63cc31fdcb166c20a6af76fed51bcaa94e4fd3f5
VinsPWD <159819760+ArvinPWD@users.noreply.github.com>
2024-04-16 01:09:50 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=MaVins Kernel by Uke@PWD
+kernel.string=Megitsune by UkePWD
 do.devicecheck=1
 do.modules=0
 do.systemless=1
