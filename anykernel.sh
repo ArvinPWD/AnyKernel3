7a41ffc3022603ef6551c7e3c03731ee526f5e76
Mavins <ukepurwadi@gmail.com>
2025-04-17 13:48:28 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Mavin kernel by PWD 
+kernel.string=Megitsune kernel by ArvinPWD 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
